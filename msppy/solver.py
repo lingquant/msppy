e78117b2d7fdb8e15de082883217b4552a92f52c
lingquant <jasonlingquanding@gmail.com>
2019-09-30 12:47:03 -0400

    default setting of reset turns to false

--- msppy/solver.py
+++ msppy/solver.py
@@ -19,11 +19,11 @@ class SDDP(object):
     MSP: list
         A multi-stage stochastic program object.
 
-    reset: bool
+    reset: bool, optional, default=0
         Whether to reset models (remove all cuts).
     """
 
-    def __init__(self, MSP, reset=True):
+    def __init__(self, MSP, reset=False):
         self.db = []
         self.pv = []
         self.cut_type = ["B"]
@@ -1198,7 +1198,7 @@ class Extensive(object):
     MSP: list
         A multi-stage stochastic program object.
 
-    reset: bool
+    reset: bool, optional, default=0
         Whether to reset models (remove all cuts)
 
     Attributes
@@ -1213,7 +1213,7 @@ class Extensive(object):
         The time cost in constructing extensive model
     """
 
-    def __init__(self, MSP, reset=True):
+    def __init__(self, MSP, reset=False):
         if reset:
             MSP._reset()
             MSP._flag_update = False
